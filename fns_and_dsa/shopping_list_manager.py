e3ebc18b61c0fe87991bcf5838b4466893e6f412
Ruth Arthur <qweba369@gmailcom>
2025-07-27 09:45:46 +0000

    add a new ffile to the folder fns and dsa

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,42 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            item = input("Enter the item you want to add: ").strip()
+            shopping_list.append(item)
+            print(f"'{item}' has been added to your shopping list.")
+            pass
+        elif choice == '2':
+            item = input("Enter the item to remove: ").strip()
+            if item in shopping_list:
+              shopping_list.remove(item)
+              print(f"'{item}' has been removed")
+            else:
+                print(f"'{item}' is not in your shopping list")
+            pass
+        elif choice == '3':
+            if shopping_list:
+              print("\nYour Shopping List:")
+              for i, item in enumerate(shopping_list, start=1):
+                   print(f"{i}, '{item}'")
+              else:
+                  print("Your shopping list is empty.")
+            pass
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
